9cdb0d4ed2a5dc8bd0856a639eb80f0fd9b83441
Jennifer Madiedo <jmadiedo@mit.edu>
2016-12-16 19:19:18 +0000

    Add files via upload

---
+++ Ros_arduinoPi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+if __name__ == '__main__':
+    rospy.init_node('lcd_screen')
+
+    environment_id = rospy.get_param("~environment_id")
+    pub_name = '/actuators/lcd_screen/cmd'
+    pub = rospy.Publisher(pub_name, String, queue_size=10)
+
+    def air_temp_callback(temp):
+        pub.publish('Air Temp: {}'.format(temp))
+
+    sub_name = "/{}/measured/air_temperature".format(environment_id)
+    sub = rospy.Subscriber(sub_name, Float64, air_temp_callback)
+
+    rospy.spin()
